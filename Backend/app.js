13693cea6c54ac8ec0d0f51bd50faf799ee95912
Aman Singh <singhaman38809@gmail.com>
2025-01-19 19:55:43 +0530

    create a user route with document

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,13 @@ const  express = require('express');
 const app = express();
 const dotenv = require('dotenv')
 dotenv.config();
+const userRoutes = require('.//routes/user.routes')
 
 const cors = require('cors')
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const connectToDb = require('./db/db');
 
 // Connect to MongoDB
@@ -13,6 +17,8 @@ connectToDb();
 app.get('/', (req, res) => {
     res.send("hello world");
 });
+
+app.use('/api/users', userRoutes);
 
 
 module.exports = app;
---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router();
+const { body } = require('express-validator')
+const userController = require('../controllers/user.controller')
+const userService = require('../services/user.service')
+
+router.post('/register',[
+    body('fullname.firstname').isLength({ min: 3 }).withMessage('Firstname length must be 3 characters long'),
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+],
+    userController.registerUser
+)
+
+
+module.exports = router;
